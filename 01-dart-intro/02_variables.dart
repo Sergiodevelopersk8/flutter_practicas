f4bcd40c76a504ffb544f0e715193accd704b0ce
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2023-12-22 17:54:25 -0600

    update

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -1,28 +1,25 @@
-void main(){
-  
-final String  pokemon = 'Charizar';
- 
-final int hp = 100;   
-  
- /*
+void main() {
+  final String pokemon = 'Charizar';
+
+  final int hp = 100;
+
+  /*
   es te tio de variable en bool puede tener 3 valores
   true false o null
   se declara asi 
   bool? isAlive;
-  */ 
-  final bool isAlive = true;  
-  
+  */
+  final bool isAlive = true;
+
   /*este es u listado */
-  final List <String> abilities = ['Fuego nitro'];
-final sprite =  <String> ['charizar/front.png','charizar/back.png'];
-  
+  final List<String> abilities = ['Fuego nitro'];
+  final sprite = <String>['charizar/front.png', 'charizar/back.png'];
+
   print("""
    $pokemon
   $pokemon
   $hp
   $abilities
-  $sprites
+  $sprite
   """);
-  
-  
 }
